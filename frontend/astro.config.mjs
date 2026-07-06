e03a0b8de1e21d5aa2f3e2fe4a45f3656a8f72e0
Andreas Nicklaus <andreas_nicklaus@web.de>
2026-07-06 22:00:47 +0200

    feat: change domain to nicklaushochzeit.de

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://hochzeit.andreasnicklaus.de',
+  site: 'https://www.nicklaushochzeit.de',
   base: '/',
   integrations: [tailwind()],
 });
